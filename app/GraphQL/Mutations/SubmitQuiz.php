facb53b2b9a40b37fdf5f97bc9cf6dbb00f801b3
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-10 17:34:06 +0530

    added submit quiz

--- app/GraphQL/Mutations/SubmitQuiz.php
+++ app/GraphQL/Mutations/SubmitQuiz.php
@@ -3,6 +3,7 @@
 namespace App\GraphQL\Mutations;
 
 use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Facades\DB;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class SubmitQuiz
@@ -11,21 +12,34 @@ class SubmitQuiz
     {
         $user = auth()->user();
 
-        $answers = collect($args['meta'])->map(function ($answer) use ($user) {
-            $points = $answer['answer'] == $answer['current_answer'] ? 10 / ($answer['seconds'] + 1) : 0;
+        $answers = collect($args['meta'])
+            ->map(function ($answer) use ($user) {
+                $points = $answer['answer'] == $answer['current_answer'] ? 10 / ($answer['seconds'] + 1) : 0;
 
-            return [
+                return [
+                    'user_id' => $user->id,
+                    'quiz_id' => $answer['quiz_id'],
+                    'question_id' => $answer['question_id'],
+                    'points' => $points,
+                    'time' => $answer['seconds'],
+                    'answer' => $answer['answer'],
+                    'attempted' => !!$answer['answer'],
+                ];
+            })
+            ->toArray();
+
+        DB::table("quiz_answers")->insert($answers);
+
+        $total_points = collect($answers)->sum('point');
+        DB::table("quiz_participants")
+            ->where([
+                'quiz_id' => $answers[0]['quiz_id'],
                 'user_id' => $user->id,
-                'quiz_id' => $answer['quiz_id'],
-                'question_id' => $answer['question_id'],
-                'points' => $points,
-                'time' => $answer['seconds'],
-                'answer' => $answer['answer'],
-                'attempted' => !!$answer['answer'],
-            ];
-        });
-
-        dump($answers);
+            ])
+            ->update([
+                'points' => $total_points,
+                'quiz_status' => 'finished',
+            ]);
 
         return true;
     }
