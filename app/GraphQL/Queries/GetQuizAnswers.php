63697ea1e859026aed25454f826d08b9c0bd95b8
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-10 19:22:19 +0530

    quiz_answer migration updated

---
+++ app/GraphQL/Queries/GetQuizAnswers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Facades\DB;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class GetQuizAnswers
+{
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        $user = auth()->user();
+
+        return DB::table('quiz_participants')
+            ->where([
+                'quiz_id' => $args['quiz_id'],
+                'user_id' => $user->id
+            ])
+            ->get();
+    }
+}
