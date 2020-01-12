ad373eb0e679a64da8216f11634c506c9c0d4649
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-12 11:33:26 +0530

    checksum hotfix

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,8 +22,25 @@ class TestController extends Controller
 
     public function test(Request $request, QuizRepositoryInterface $quizRepo)
     {
-        $checksum = "OmVfj0nkFfMq8Sy6yXq8sihW+Mtuc/Aar7+KWpW6q9KwYJRoyh+44Jf3KS5ko6y00W/XjtB4Nxtt89/Va/eipxCHBIDRF3TrmDGGnoOmxas=";
+        $checksum = "ucXLtY2ipOTqrlIfR3PfjABANEXOFM8b+JRrEhO7OE30ysf6zv1Oia3yXmLkff7CeR9aq4w+3RdDSAoYFMrYYL39bfpWMxsAFs2qHBIoVO4=";
 
-        return compact('checksum');
+        $data = [
+            "MID" => "dBhsxy51569465348988",
+            "ORDER_ID" => "30698f4a-7d7f-42af-b9f5-2cf05b90df88",
+            "CALLBACK_URL" => "https=>//securegw.paytm.in/theia/paytmCallback?ORDER_ID=30698f4a-7d7f-42af-b9f5-2cf05b90df88",
+            "CHANNEL_ID" => "WAP",
+            "CUST_ID" => "11baadeb-09c4-4b2d-8558-37695dd98756",
+            "INDUSTRY_TYPE_ID" => "Retail",
+            "TXN_AMOUNT" => "10",
+            "WEBSITE" => "WEBSTAGING"
+        ];
+
+        if ($request->type === "verify") {
+            $verify = verifychecksum_e($data, env('PAYTM_MERCHANT_KEY'), $checksum);
+            return compact('verify');
+        }
+
+        $generate = getChecksumFromArray($data, env('PAYTM_MERCHANT_KEY'));
+        return compact('generate');
     }
 }
