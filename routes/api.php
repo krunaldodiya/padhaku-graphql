92460c282e7b82f114a6497f847614a6b8a26ebe
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-16 12:40:24 +0530

    checksum hotfix

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->post('/checksum', function (Request $request) {
+    $data = [
+        "MID" => $request->get('MID'),
+        "ORDER_ID" => $request->get('ORDER_ID'),
+        "CALLBACK_URL" => $request->get('CALLBACK_URL'),
+        "CHANNEL_ID" => $request->get('CHANNEL_ID'),
+        "CUST_ID" => $request->get('CUST_ID'),
+        "INDUSTRY_TYPE_ID" => $request->get('INDUSTRY_TYPE_ID'),
+        "TXN_AMOUNT" => $request->get('TXN_AMOUNT'),
+        "MOBILE_NO" => $request->get('MOBILE_NO'),
+        "EMAIL" => $request->get('EMAIL'),
+        "WEBSITE" => $request->get('WEBSITE'),
+    ];
+
+    return getChecksumFromArray($data, env('PAYTM_MERCHANT_KEY'));
+});
