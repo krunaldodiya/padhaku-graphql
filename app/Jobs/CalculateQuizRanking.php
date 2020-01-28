8ef6b4129be6a2e6436c31b973111b2e7ef4428e
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-28 13:01:11 +0530

    added info

--- app/Jobs/CalculateQuizRanking.php
+++ app/Jobs/CalculateQuizRanking.php
@@ -62,7 +62,7 @@ class CalculateQuizRanking implements ShouldQueue
 
         DB::table("quiz_rankings")->insert($quiz_rankings->toArray());
 
-        $quiz_data->participants()->where('quiz_status', '!=', 'started')->update(['status' => 'left']);
+        DB::table("quiz_participants")->where('quiz_status', '==', 'joined')->update(['quiz_status' => 'left']);
 
         $quiz_rankings->each(function ($quiz_ranking) {
             $user = User::find($quiz_ranking['user_id']);
