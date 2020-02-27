aa4af48fff56c35a9b8b906457347c0626da6df7
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-02-27 15:21:44 +0530

    reset database

---
+++ app/GraphQL/Queries/GetQuizWinners.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\QuizRanking;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class GetQuizWinners
+{
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        return QuizRanking::where('quiz_id', $args['quiz_id'])->get();
+    }
+}
