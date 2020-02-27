ba19eaec4a316884d4669ebc01a0b65edad473d8
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-02-27 16:09:57 +0530

    reset database

--- app/QuizRanking.php
+++ app/QuizRanking.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class QuizRanking extends Model
 {
@@ -12,6 +13,20 @@ class QuizRanking extends Model
     public $incrementing = false;
 
     protected $guarded = [];
+
+    protected $appends = [
+        'score'
+    ];
+
+    public function getScoreAttribute()
+    {
+        $quiz_participants = DB::table('quiz_participants')
+            ->where('user_id', auth()->id)
+            ->where('quiz_id', $this->quiz_id)
+            ->first();
+
+        return $quiz_participants ? $quiz_participants->points : 0;
+    }
 
     public function quiz()
     {
