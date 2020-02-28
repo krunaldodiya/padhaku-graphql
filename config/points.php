d40172912327489b9c2c188f377ddcf0642a8981
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-02-28 09:28:18 +0530

    reset database

--- config/points.php
+++ config/points.php
@@ -2,17 +2,17 @@
 
 return [
     'user_registered' => [
-        'type' => 'user_registered',
+        'type' => 'Joining Bonus',
         'points' => 50
     ],
 
     'profile_updated' => [
-        'type' => 'profile_updated',
+        'type' => 'Profile Updated',
         'points' => 5
     ],
 
     'invitation' => [
-        'type' => 'invitation',
+        'type' => 'Invitation',
         'points' => 5
     ],
 ];
