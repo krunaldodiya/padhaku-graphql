94215e781f6ffa215963a7cf59a7d0a74333494d
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-02-29 14:08:57 +0530

    reset database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Invitation;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -18,5 +19,22 @@ class UserController extends Controller
         User::where('id', $user->id)->update(['avatar' => $filename]);
 
         return response(['filename' => $filename], 200);
+    }
+
+    public function checkInvitation(Request $request)
+    {
+        $sender_id = $request->segment(3);
+        $mobile = $request->segment(4);
+
+        $exists = Invitation::where(['mobile' => $mobile])->first();
+
+        if (!$exists) {
+            Invitation::create([
+                'sender_id' => $sender_id,
+                'mobile' => $mobile
+            ]);
+        }
+
+        return redirect("https://play.google.com/store/apps/details?id=com.pauzr.org");
     }
 }
