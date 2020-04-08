8e35a218ad03370774cae59ec451338508569f23
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-08 19:53:50 +0530

    added relationships

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -25,108 +25,119 @@
 </head>
 
 <body>
-    <div class="container">
-        <h2>Privacy Policy</h2>
-        <p>Pauzr built the Sawaal Bemisaal app as a Free app. This SERVICE is provided by Pauzr at no cost and is
-            intended for
-            use as is.</p>
-        <p>This page is used to inform visitors regarding our policies with the collection, use, and disclosure of
-            Personal
-            Information if anyone decided to use our Service.</p>
-        <p>If you choose to use our Service, then you agree to the collection and use of information in relation to this
-            policy.
-            The Personal Information that we collect is used for providing and improving the Service. We will not
-            use or
-            share your information with anyone except as described in this Privacy Policy.</p>
-        <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is
-            accessible at
-            Sawaal Bemisaal unless otherwise defined in this Privacy Policy.</p>
-        <p><strong>Information Collection and Use</strong></p>
-        <p>For a better experience, while using our Service, we may require you to provide us with certain personally
-            identifiable information, including but not limited to Mobile Number,Contact,Name,Email. The information
-            that we
-            request will be retained by us and used as described in this privacy policy.</p>
-        <p>The app does use third party services that may collect information used to identify you.</p>
-        <p>Link to privacy policy of third party service providers used by the app</p>
-        <ul>
-            <li><a href="https://www.google.com/policies/privacy/">Google Play Services</a></li>
-            <li><a href="https://firebase.google.com/policies/analytics">Google Analytics for Firebase</a></li>
-        </ul>
-        <p><strong>App Permissions</strong></p>
-        <p>The app requests below permissions from the user -</p>
-        <ul>
-            <li>CAMERA</li>
-            <li>READ_CONTACTS</li>
-        </ul>
-        <p>CAMERA permission is requested to allow users to upload their profile image using phone&rsquo;s camera.</p>
-        <p>READ_CONTACTS permission is requested to allow users to invite their other friends to use the app from their
-            Phone
-            Contacts.</p>
-        <p><strong>Log Data</strong></p>
-        <p>We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and
-            information (through third party products) on your phone called Log Data. This Log Data may include
-            information
-            such as your device Internet Protocol (&ldquo;IP&rdquo;) address, device name, operating system version,
-            the
-            configuration of the app when utilizing our Service, the time and date of your use of the Service, and
-            other
-            statistics.</p>
-        <p><strong>Cookies</strong></p>
-        <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These
-            are sent
-            to your browser from the websites that you visit and are stored on your device's internal memory.</p>
-        <p>This Service does not use these &ldquo;cookies&rdquo; explicitly. However, the app may use third party code
-            and
-            libraries that use &ldquo;cookies&rdquo; to collect information and improve their services. You have the
-            option
-            to either accept or refuse these cookies and know when a cookie is being sent to your device. If you
-            choose to
-            refuse our cookies, you may not be able to use some portions of this Service.</p>
-        <p><strong>Service Providers</strong></p>
-        <p>We may employ third-party companies and individuals due to the following reasons:</p>
-        <ul>
-            <li>To facilitate our Service;</li>
-            <li>To provide the Service on our behalf;</li>
-            <li>To perform Service-related services; or</li>
-            <li>To assist us in analyzing how our Service is used.</li>
-        </ul>
-        <p>We want to inform users of this Service that these third parties have access to your Personal Information.
-            The reason
-            is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or
-            use the
-            information for any other purpose.</p>
-        <p><strong>Security</strong></p>
-        <p>We value your trust in providing us your Personal Information, thus we are striving to use commercially
-            acceptable
-            means of protecting it. But remember that no method of transmission over the internet, or method of
-            electronic
-            storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
-        <p><strong>Links to Other Sites</strong></p>
-        <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to
-            that site.
-            Note that these external sites are not operated by us. Therefore, we strongly advise you to review the
-            Privacy
-            Policy of these websites. We have no control over and assume no responsibility for the content, privacy
-            policies, or practices of any third-party sites or services.</p>
-        <p><strong>Children&rsquo;s Privacy</strong></p>
-        <p>These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable
-            information from children under 13. In the case we discover that a child under 13 has provided us with
-            personal
-            information, we immediately delete this from our servers. If you are a parent or guardian and you are
-            aware that
-            your child has provided us with personal information, please contact us so that we will be able to do
-            necessary
-            actions.</p>
-        <p><strong>Changes to This Privacy Policy</strong></p>
-        <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically
-            for any
-            changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes
-            are
-            effective immediately after they are posted on this page.</p>
-        <p><strong>Contact Us</strong></p>
-        <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at
-            sawaalbemisaal@gmail.com.</p>
-    </div>
+    <h2><strong>Privacy Policy</strong></h2>
+    <p><span style="font-weight: 400;">Last Modified Date: 7th April 2020</span></p>
+    <p><span style="font-weight: 400;">Sawaal Bemisaal app is a Free app. This SERVICE is provided by Sawaal Bemisaal at
+            no cost and is intended for use as is.</span></p>
+    <p><span style="font-weight: 400;">This page is used to inform visitors regarding our policies with the collection,
+            use, and disclosure of Personal Information if anyone decided to use our Service.</span></p>
+    <p><span style="font-weight: 400;">If you choose to use our Service, then you agree to the collection and use of
+            information in relation to this policy. The Personal Information that we collect is used for providing and
+            improving the Service. We will not use or share your information with anyone except as described in this
+            Privacy Policy.&nbsp;</span></p>
+    <p><span style="font-weight: 400;">The terms used in this Privacy Policy have the same meanings as in our Terms and
+            Conditions, which is accessible at Sawaal Bemisaal unless otherwise defined in this Privacy Policy.</span>
+    </p>
+    <p><strong>Information Collection and Use</strong></p>
+    <p><span style="font-weight: 400;">For a better experience, while using our Service, we may require you to provide
+            us with certain personally identifiable information, including but not limited to Mobile Number, Contact,
+            Name, Email. The information that we request will be retained by us and used as described in this privacy
+            policy.</span></p>
+    <ol>
+        <li style="font-weight: 400;"><span style="font-weight: 400;">Mobile Number:&nbsp;</span></li>
+    </ol>
+    <p><span style="font-weight: 400;">(i) Mobile number is also used in assisting you in case of any account related
+            queries raised by you.</span></p>
+    <p><span style="font-weight: 400;">(ii) Mobile number is also used to communicate account related activities and
+            custom offers occasionally.</span></p>
+    <ol>
+        <li style="font-weight: 400;"><span style="font-weight: 400;">Email address:&nbsp;</span></li>
+    </ol>
+    <p><span style="font-weight: 400;">&nbsp;&nbsp;&nbsp;&nbsp;(i) Your email address is used to communicate service
+            alerts (your activity on Sawaal Bemisaal &amp; Service related communication).</span></p>
+    <p><span style="font-weight: 400;">In case you choose to decline to submit your mobile number on the app during
+            registration, we will not be able to provide services on the app to you. We will make reasonable efforts to
+            notify you of the same at the time of opening your account.</span></p>
+    <p><span style="font-weight: 400;">When you register with the Sawaal Bemisaal, we contact you from time to time
+            about updating your personal information to provide you features that we believe may benefit / interest you.
+            By agreeing to this privacy policy, you explicitly give us consent to use your information as described in
+            this privacy policy. </span><strong>If you do not agree, please do not use or access Sawaal Bemisaal
+            services</strong></p>
+    <p><span style="font-weight: 400;">The app does use third party services that may collect information used to
+            identify you.</span></p>
+    <p><span style="font-weight: 400;">Link to privacy policy of third party service providers used by the app</span>
+    </p>
+    <ul>
+        <li style="font-weight: 400;"><a href="https://www.google.com/policies/privacy/"><span
+                    style="font-weight: 400;">Google Play Services</span></a></li>
+        <li style="font-weight: 400;"><a href="https://firebase.google.com/policies/analytics"><span
+                    style="font-weight: 400;">Google Analytics for Firebase</span></a></li>
+    </ul>
+    <p><strong>App Permissions</strong></p>
+    <p><span style="font-weight: 400;">The app requests below permissions from the user -</span></p>
+    <ul>
+        <li style="font-weight: 400;"><span style="font-weight: 400;">CAMERA</span></li>
+        <li style="font-weight: 400;"><span style="font-weight: 400;">READ_CONTACTS</span></li>
+    </ul>
+    <p><span style="font-weight: 400;">CAMERA permission is requested to allow users to upload their profile image using
+            the phone's camera or phone Gallery</span></p>
+    <p><span style="font-weight: 400;">READ_CONTACTS permission is requested to allow users to invite their other
+            friends to use the app from their Phone Contacts.</span></p>
+    <p><strong>Log Data</strong></p>
+    <p><span style="font-weight: 400;">We want to inform you that whenever you use our Service, in a case of an error in
+            the app we collect data and information (through third party products) on your phone called Log Data. This
+            Log Data may include information such as your device Internet Protocol (&ldquo;IP&rdquo;) address, device
+            name, operating system version, the configuration of the app when utilizing our Service, the time and date
+            of your use of the Service, and other statistics.</span></p>
+    <p><strong>Cookies</strong></p>
+    <p><span style="font-weight: 400;">Cookies are files with a small amount of data that are commonly used as anonymous
+            unique identifiers. These are sent to your browser from the websites that you visit and are stored on your
+            device's internal memory.</span></p>
+    <p><span style="font-weight: 400;">This Service does not use these &ldquo;cookies&rdquo; explicitly. However, the
+            app may use third party code and libraries that use &ldquo;cookies&rdquo; to collect information and improve
+            their services. You have the option to either accept or refuse these cookies and know when a cookie is being
+            sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this
+            Service.</span></p>
+    <p><strong>Service Providers</strong></p>
+    <p><span style="font-weight: 400;">We may employ third-party companies and individuals due to the following
+            reasons:</span></p>
+    <ul>
+        <li style="font-weight: 400;"><span style="font-weight: 400;">To facilitate our Service;</span></li>
+        <li style="font-weight: 400;"><span style="font-weight: 400;">To provide the Service on our behalf;</span></li>
+        <li style="font-weight: 400;"><span style="font-weight: 400;">To perform Service-related services; or</span>
+        </li>
+        <li style="font-weight: 400;"><span style="font-weight: 400;">To assist us in analyzing how our Service is
+                used.</span></li>
+    </ul>
+    <p><span style="font-weight: 400;">We want to inform users of this Service that these third parties have access to
+            your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they
+            are obligated not to disclose or use the information for any other purpose.</span></p>
+    <p><strong>Security</strong></p>
+    <p><span style="font-weight: 400;">We value your trust in providing us your Personal Information, thus we are
+            striving to use commercially acceptable means of protecting it. But remember that no method of transmission
+            over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its
+            absolute security.</span></p>
+    <p><strong>Links to Other Sites</strong></p>
+    <p><span style="font-weight: 400;">This Service may contain links to other sites. If you click on a third-party
+            link, you will be directed to that site. Note that these external sites are not operated by us. Therefore,
+            we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no
+            responsibility for the content, privacy policies, or practices of any third-party sites or services.</span>
+    </p>
+    <p><strong>Children&rsquo;s Privacy</strong></p>
+    <p><span style="font-weight: 400;">These Services do not address anyone under the age of 18. We do not knowingly
+            collect personally identifiable information from children under 18. In the case we discover that a user
+            under 18 has provided us with personal information, we immediately delete this from our servers. If you are
+            a parent or guardian and you are aware that your child has provided us with personal information, please
+            contact us so that we will be able to do necessary actions.</span></p>
+    <p><strong>Changes to This Privacy Policy</strong></p>
+    <p><span style="font-weight: 400;">We may update our Privacy Policy from time to time. Thus, you are advised to
+            review this page periodically for any changes. We will notify you of any changes by posting the new Privacy
+            Policy on this page. These changes are effective immediately after they are posted on this page.</span></p>
+    <p><strong>Grievance Contact Information</strong></p>
+    <p><span style="font-weight: 400;">If you have any questions or suggestions about our Privacy Policy, do not
+            hesitate to contact us at sawaalbemisaal@gmail.com or support@sawalbemisaal.com</span></p>
+    <p><span style="font-weight: 400;">&nbsp;</span></p>
+    <p>&nbsp;</p>
 </body>
 
 </html>
