be565932777a1847d8ad742fab18ea76b586f1aa
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-22 18:58:12 +0530

    added release

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('create:quiz')->cron('0 */2 * * *');
+        $schedule->command('create:quiz')->everyThirtyMinutes();
     }
 
     /**
