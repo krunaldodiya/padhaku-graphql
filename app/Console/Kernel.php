8649affc02c81e1f2f52fd87b252f7f5f617c4da
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-27 19:25:06 +0530

    bulk quiz generation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('create:quiz')->dailyAt('13:00');
+        $schedule->command('create:quiz')->dailyAt('01:00');
     }
 
     /**
