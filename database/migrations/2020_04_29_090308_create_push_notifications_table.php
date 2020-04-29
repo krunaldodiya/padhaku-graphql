2451de700c839fedc60386d185b79a7e35331230
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-29 09:09:19 +0530

    ordered quiz by expiry

---
+++ database/migrations/2020_04_29_090308_create_push_notifications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePushNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('push_notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->string('subject');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+
+            $table->uuid('topic_id');
+            $table->foreign('topic_id')->references('id')->on('topics')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('push_notifications');
+    }
+}
