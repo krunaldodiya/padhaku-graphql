49fd6990fe2eb3ca2e2d2e745860b07c313d89a3
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-29 07:56:58 +0530

    codepush auto on app open

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('create:quiz')->dailyAt('8:00');
+        $schedule->command('create:quiz')->dailyAt('08:00');
         // $schedule->command('create:quiz')->dailyAt('01:00');
     }
 
