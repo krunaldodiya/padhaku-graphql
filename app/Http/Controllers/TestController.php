81894c46d7f8f50c616de07fb34ab0d28f0e94f8
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-04-30 13:14:11 +0530

    added refer system

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -20,13 +20,19 @@ class TestController extends Controller
     {
         $client = new FcmClient(env('FIREBASE_SERVER_KEY'), env('FIREBASE_SENDER_ID'));
 
-        // $user = User::where('email', "kunal.dodiya1@gmail.com")->first();
+        if ($request->subscribe) {
 
-        // foreach ($user->device_tokens as $device_token) {
-        //     $client->topicSubscribe('testing', $device_token->token);
-        // }
+            // $user = User::where('email', "kunal.dodiya1@gmail.com")->first();
 
-        $subscribe = $client->topicSubscribe("testing", "dKbQw6QfBEo:APA91bEMxB5vUhQkMaIAhyw9hwzOeHM3b1Jt24svjmD72TkDGmGyPvRd8fWn52DLu_wfwxDFercbHOibWoWNktmdu3Hkkv746E-VnSAUscOTmd_2rMCCT16iOiwgNJ_h0hsH_eoyaI4L");
-        dd($subscribe);
+            // foreach ($user->device_tokens as $device_token) {
+            //     $client->topicSubscribe('testing', $device_token->token);
+            // }
+
+            $client->topicSubscribe("testing", "dKbQw6QfBEo:APA91bEMxB5vUhQkMaIAhyw9hwzOeHM3b1Jt24svjmD72TkDGmGyPvRd8fWn52DLu_wfwxDFercbHOibWoWNktmdu3Hkkv746E-VnSAUscOTmd_2rMCCT16iOiwgNJ_h0hsH_eoyaI4L");
+        }
+
+        if ($request->notify) {
+            $client->pushNotification("testing", "test", "/topics/testing");
+        }
     }
 }
