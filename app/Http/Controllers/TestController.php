2c73544d2c7646738d8c84fb80a17fc1a829a565
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-05-01 18:04:42 +0530

    added user info in nova

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Question;
 use App\User;
 use Fcm\FcmClient;
 use Illuminate\Http\Request;
@@ -18,22 +19,8 @@ class TestController extends Controller
 
     public function test(Request $request)
     {
-        $client = new FcmClient(env('FIREBASE_SERVER_KEY'), env('FIREBASE_SENDER_ID'));
-        $device_id = "dKbQw6QfBEo:APA91bEMxB5vUhQkMaIAhyw9hwzOeHM3b1Jt24svjmD72TkDGmGyPvRd8fWn52DLu_wfwxDFercbHOibWoWNktmdu3Hkkv746E-VnSAUscOTmd_2rMCCT16iOiwgNJ_h0hsH_eoyaI4L";
+        $questions =  Question::where('answer', 'option_5')->update(['answer' => 'option_4']);
 
-        if ($request->subscribe) {
-
-            // $user = User::where('email', "kunal.dodiya1@gmail.com")->first();
-
-            // foreach ($user->device_tokens as $device_token) {
-            //     $client->topicSubscribe('testing', $device_token->token);
-            // }
-
-            $client->topicSubscribe("testing", $device_id);
-        }
-
-        if ($request->notify) {
-            $client->pushNotification("testing", "test", $device_id);
-        }
+        dd($questions);
     }
 }
