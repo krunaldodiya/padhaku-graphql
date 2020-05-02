ada863f3e36637eafc99896fcf1193e94df72602
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-05-02 14:55:59 +0530

    added topic

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Topic;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -17,8 +18,15 @@ class TestController extends Controller
 
     public function test(Request $request)
     {
-        $user = User::first();
+        $users = User::get();
 
-        return $user;
+        foreach ($users as $user) {
+            $public_topic = Topic::where(["name" => "user_all"])->first();
+            $public_topic->subscribers()->attach($user);
+
+            $private_topic = Topic::create(["name" => "user_{$user->id}"]);
+            $private_topic->subscribers()->attach($user);
+        }
+        return "done";
     }
 }
