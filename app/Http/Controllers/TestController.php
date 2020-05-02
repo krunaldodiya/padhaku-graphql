9aba116966831176cf3a473d3d082357728a1052
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-05-02 16:52:36 +0530

    added topic

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Quiz;
 use App\Topic;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -18,11 +19,17 @@ class TestController extends Controller
 
     public function test(Request $request)
     {
-        $topics = Topic::query()
-            ->where('name', 'LIKE', 'quiz_reminder_%')
-            ->whereDate('created_at', "!=", Carbon::today())
-            ->delete();
+        $quizzes = Quiz::query()
+            ->whereDate('created_at', "=", Carbon::today())
+            ->whereDate('expired_at', '<=', "2020-05-02 16:50:00")
+            ->get();
 
-        return $topics;
+        foreach ($quizzes as $quiz) {
+            Topic::query()
+                ->where('name', "quiz_reminder_{$quiz->id}")
+                ->delete();
+        }
+
+        return "done";
     }
 }
